822de5e0a2e57ff4f5e295cda8bc87a362a7bf57
vivek-outdid <vivek@outdidtech.com>
2025-05-22 12:16:09 +0530

    initial setup

---
+++ BACKEND/app.js
@@ -0,0 +1,10 @@
+// EXAMPLE CODE
+// const express = require('express');
+// const app = express();
+// const websiteRoutes = require('./routes/websiteRoutes');
+// const adminRoutes = require('./routes/adminRoutes');
+// const appRoutes = require('./routes/appRoutes');
+
+// app.use('/api/website', websiteRoutes);
+// app.use('/api/admin', adminRoutes);
+// app.use('/api/app', appRoutes);
