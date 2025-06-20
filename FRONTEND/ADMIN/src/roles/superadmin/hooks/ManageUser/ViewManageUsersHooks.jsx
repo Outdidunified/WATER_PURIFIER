c6b520eee02e8d4054865d78f1e0d9563346420e
Sujatavilaskagle <sujata@outdidtech.com>
2025-06-20 11:17:29 +0530

    updated the controllers

--- FRONTEND/ADMIN/src/roles/superadmin/hooks/ManageUser/ViewManageUsersHooks.jsx
+++ FRONTEND/ADMIN/src/roles/superadmin/hooks/ManageUser/ViewManageUsersHooks.jsx
@@ -1,4 +1,3 @@
-//ViewManageUsers
 import { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 
@@ -14,7 +13,11 @@ const useViewManageUser = () => {
     password: '',
     city: '',
     createdby: '',
+    modifiedby: '',
     createdDate: '',
+    modifiedDate: '',
+    createddate: '',
+    modifieddate: '',
     status: false,
     role_id: '',
     role_name: '',
@@ -25,9 +28,14 @@ const useViewManageUser = () => {
     active_order_id: '',
     active_plan_id: '',
     assigned_device_id: '',
+    assigned_device_ids: [],
     subscribed_at: '',
     subscription_expiry_date: '',
+    otp: '',
     otpExpires: '',
+    otpGeneratedAt: '',
+    security_deposit: '',
+    security_deposit_added: false,
   });
 
   useEffect(() => {
@@ -41,7 +49,11 @@ const useViewManageUser = () => {
         password: dataItem.password || '',
         city: dataItem.city || '',
         createdby: dataItem.createdby || '',
+        modifiedby: dataItem.modifiedby || '',
         createdDate: dataItem.createdDate || '',
+        modifiedDate: dataItem.modifiedDate || '',
+        createddate: dataItem.createddate || '',
+        modifieddate: dataItem.modifieddate || '',
         status: dataItem.status || false,
         role_id: dataItem.role_id || '',
         role_name: dataItem.role_name || '',
@@ -52,16 +64,52 @@ const useViewManageUser = () => {
         active_order_id: dataItem.active_order_id || '',
         active_plan_id: dataItem.active_plan_id || '',
         assigned_device_id: dataItem.assigned_device_id || '',
+        assigned_device_ids: dataItem.assigned_device_ids || [],
         subscribed_at: dataItem.subscribed_at || '',
         subscription_expiry_date: dataItem.subscription_expiry_date || '',
+        otp: dataItem.otp || '',
         otpExpires: dataItem.otpExpires || '',
+        otpGeneratedAt: dataItem.otpGeneratedAt || '',
+        security_deposit: dataItem.security_deposit || '',
+        security_deposit_added: dataItem.security_deposit_added || false,
       });
 
       localStorage.setItem('userData', JSON.stringify(dataItem));
     } else {
       const savedData = JSON.parse(localStorage.getItem('userData'));
       if (savedData) {
-        setUser(savedData);
+        setUser({
+          _id: savedData._id || '',
+          name: savedData.name || '',
+          email: savedData.email || '',
+          phone: savedData.phone || '',
+          password: savedData.password || '',
+          city: savedData.city || '',
+          createdby: savedData.createdby || '',
+          modifiedby: savedData.modifiedby || '',
+          createdDate: savedData.createdDate || '',
+          modifiedDate: savedData.modifiedDate || '',
+          createddate: savedData.createddate || '',
+          modifieddate: savedData.modifieddate || '',
+          status: savedData.status || false,
+          role_id: savedData.role_id || '',
+          role_name: savedData.role_name || '',
+          user_id: savedData.user_id || '',
+          is_subscribed: savedData.is_subscribed || false,
+          active_duration_id: savedData.active_duration_id || '',
+          active_label: savedData.active_label || '',
+          active_order_id: savedData.active_order_id || '',
+          active_plan_id: savedData.active_plan_id || '',
+          assigned_device_id: savedData.assigned_device_id || '',
+          assigned_device_ids: savedData.assigned_device_ids || [],
+          subscribed_at: savedData.subscribed_at || '',
+          subscription_expiry_date: savedData.subscription_expiry_date || '',
+          otp: savedData.otp || '',
+          otpExpires: savedData.otpExpires || '',
+          otpGeneratedAt: savedData.otpGeneratedAt || '',
+          security_deposit: savedData.security_deposit || '',
+          security_deposit_added: savedData.security_deposit_added || false,
+        });
       }
     }
   }, [location]);
--- FRONTEND/ADMIN/src/roles/superadmin/page/ManageUser/ViewManageUser.jsx
+++ FRONTEND/ADMIN/src/roles/superadmin/page/ManageUser/ViewManageUser.jsx
@@ -12,93 +12,111 @@ const ViewManageUser = ({ userInfo, handleLogout }) => {
 
   
 
-    return (
-        <div className='container-scroller'>
-            <Header userInfo={userInfo} handleLogout={handleLogout} />
-            <div className="container-fluid page-body-wrapper">
-                <Sidebar />
-                <div className="main-panel">
-                    <div className="content-wrapper">
-                        <div className="row">
-                            <div className="col-md-12 grid-margin">
-                                <div className="row">
-                                    <div className="col-12 col-xl-8 mb-4 mb-xl-0">
-                                        <h3 className="font-weight-bold">Manage User's</h3>
-                                    </div>
-                                    <div className="col-12 col-xl-4">
-                                        <div className="justify-content-end d-flex">
-                                            <button type="button" className="btn btn-outline-primary btn-icon-text" onClick={() => handleEditUser(user)} style={{ marginRight: '10px' }}>
-                                                <i className="mdi mdi-pencil btn-icon-prepend"></i>Edit
-                                            </button>
-                                            <button type="button" className="btn btn-success" onClick={handleBack}>Back</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="row">
-                            <div className="col-lg-12 grid-margin stretch-card">
-                                <div className="card">
-                                    <div className="card-body">
-                                                <h4 className="card-title" style={{ textAlign: 'center', paddingBottom: '10px' }}>User Details</h4>
-                                                <hr />
-
-                                            <div className="row col-12 viewDataCss">
-                                                <div className="col-md-4"><strong>User Name:</strong> {user.name || '-'}</div>
-                                                <div className="col-md-4"><strong>Email Id:</strong> {user.email || '-'}</div>
-                                                <div className="col-md-4"><strong>Phone Number:</strong> {user.phone || '-'}</div>
-                                            </div>
-
-                                            <div className="row col-12 viewDataCss">
-                                                <div className="col-md-4"><strong>Password:</strong> {user.password || '-'}</div>
-                                                <div className="col-md-4"><strong>City:</strong> {user.city || '-'}</div>
-<div className="col-md-4">
-  <strong>Role:</strong> {
-    user.role_id === 1 ? 'Admin' :
-    user.role_id === 2 ? 'Technician' :
-    user.role_id === 3 ? 'End User' :
-    'Unknown'
-  }
-</div>
-                                            </div>
-
-                                            <div className="row col-12 viewDataCss">
-                                                <div className="col-md-4"><strong>Assigned Device ID:</strong> {user.assigned_device_id || '-'}</div>
-                                                <div className="col-md-4"><strong>User ID:</strong> {user.user_id || '-'}</div>
-                                                <div className="col-md-4"><strong>Status:</strong> {user.status ? <span className="text-success">Active</span> : <span className="text-danger">DeActive</span>}</div>
-                                            </div>
-
-                                            <div className="row col-12 viewDataCss">
-                                                <div className="col-md-4"><strong>Is Subscribed:</strong> {user.is_subscribed ? 'Yes' : 'No'}</div>
-                                                <div className="col-md-4"><strong>Active Plan:</strong> {user.active_label || '-'}</div>
-                                                <div className="col-md-4"><strong>Active Duration:</strong> {user.active_duration_id || '-'}</div>
-                                            </div>
-
-                                            <div className="row col-12 viewDataCss">
-                                                <div className="col-md-4"><strong>Subscribed At:</strong> {user.subscribed_at ? formatTimestamp(user.subscribed_at) : '-'}</div>
-                                                <div className="col-md-4"><strong>Subscription Expiry Date:</strong> {user.subscription_expiry_date ? formatTimestamp(user.subscription_expiry_date) : '-'}</div>
-                                                <div className="col-md-4"><strong>Active Order ID:</strong> {user.active_order_id || '-'}</div>
-                                            </div>
-
-                                            <div className="row col-12 viewDataCss">
-                                                <div className="col-md-4"><strong>Created By:</strong> {user.createdby || '-'}</div>
-                                                <div className="col-md-4"><strong>Created Date:</strong> {user.createdDate ? formatTimestamp(user.createdDate) : '-'}</div>
-                                                <div className="col-md-4"><strong>OTP Expires:</strong> {user.otpExpires ? formatTimestamp(user.otpExpires) : '-'}</div>
-                                            </div>
-
-                                          
-                                        </div>
-                                    </div>
-                                
-                            </div>
-                        </div>
-                    </div>
-                    <Footer />
+  return (
+  <div className="container-scroller">
+    <Header userInfo={userInfo} handleLogout={handleLogout} />
+    <div className="container-fluid page-body-wrapper">
+      <Sidebar />
+      <div className="main-panel">
+        <div className="content-wrapper">
+          <div className="row">
+            <div className="col-md-12 grid-margin">
+              <div className="row">
+                <div className="col-12 col-xl-8 mb-4 mb-xl-0">
+                  <h3 className="font-weight-bold">Manage User's</h3>
                 </div>
+                <div className="col-12 col-xl-4">
+                  <div className="justify-content-end d-flex">
+                    <button type="button" className="btn btn-outline-primary btn-icon-text" onClick={() => handleEditUser(user)} style={{ marginRight: '10px' }}>
+                      <i className="mdi mdi-pencil btn-icon-prepend"></i>Edit
+                    </button>
+                    <button type="button" className="btn btn-success" onClick={handleBack}>Back</button>
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
+
+          <div className="row">
+            <div className="col-lg-12 grid-margin stretch-card">
+              <div className="card">
+                <div className="card-body">
+                  <h4 className="card-title text-center pb-3">User Details</h4>
+                  <hr />
+
+                  <div className="row col-12 viewDataCss">
+                    <div className="col-md-4"><strong>User Name:</strong> {user.name || '-'}</div>
+                    <div className="col-md-4"><strong>Email Id:</strong> {user.email || '-'}</div>
+                    <div className="col-md-4"><strong>Phone Number:</strong> {user.phone || '-'}</div>
+                  </div>
+
+                  <div className="row col-12 viewDataCss">
+                    <div className="col-md-4"><strong>Password:</strong> {user.password || '-'}</div>
+                    <div className="col-md-4"><strong>City:</strong> {user.city || '-'}</div>
+                    <div className="col-md-4">
+                      <strong>Role:</strong> {
+                        user.role_id === 1 ? 'Admin' :
+                        user.role_id === 2 ? 'Technician' :
+                        user.role_id === 3 ? 'End User' : 'Unknown'
+                      }
+                    </div>
+                  </div>
+
+                  <div className="row col-12 viewDataCss">
+                    <div className="col-md-4"><strong>User ID:</strong> {user.user_id || '-'}</div>
+                    <div className="col-md-4"><strong>Status:</strong> {user.status ? <span className="text-success">Active</span> : <span className="text-danger">DeActive</span>}</div>
+                                        <div className="col-md-4"><strong>Created By:</strong> {user.createdby || '-'}</div>
+
+                  </div>
+
+                  {/* End User Specific Fields */}
+                  {user.role_id === 3 && (
+                    <>
+                      <div className="row col-12 viewDataCss">
+                        <div className="col-md-4"><strong>Is Subscribed:</strong> {user.is_subscribed ? 'Yes' : 'No'}</div>
+                        <div className="col-md-4"><strong>Active Plan:</strong> {user.active_label || '-'}</div>
+                        <div className="col-md-4"><strong>Active Plan ID:</strong> {user.active_plan_id || '-'}</div>
+                      </div>
+
+                      <div className="row col-12 viewDataCss">
+                        <div className="col-md-4"><strong>Active Duration ID:</strong> {user.active_duration_id || '-'}</div>
+                        <div className="col-md-4"><strong>Active Order ID:</strong> {user.active_order_id || '-'}</div>
+                        <div className="col-md-4"><strong>Security Deposit:</strong> ₹{user.security_deposit || 0}</div>
+                      </div>
+
+                      <div className="row col-12 viewDataCss">
+                    <div className="col-md-4"><strong>Assigned Device ID(s):</strong> {user.assigned_device_ids?.join(', ') || user.assigned_device_id || '-'}</div>
+                        <div className="col-md-4"><strong>Subscribed At:</strong> {user.subscribed_at ? formatTimestamp(user.subscribed_at) : '-'}</div>
+                        <div className="col-md-4"><strong>Subscription Expiry Date:</strong> {user.subscription_expiry_date ? formatTimestamp(user.subscription_expiry_date) : '-'}</div>
+                      </div>
+                    </>
+                  )}
+
+                  {/* Common for All Roles if Present */}
+                  <div className="row col-12 viewDataCss">
+                    <div className="col-md-4"><strong>OTP Generated At:</strong> {user.otpGeneratedAt ? formatTimestamp(user.otpGeneratedAt) : '-'}</div>
+
+                    <div className="col-md-4"><strong>Modified By:</strong> {user.modifiedby || '-'}</div>
+                    <div className="col-md-4"><strong>OTP Expires:</strong> {user.otpExpires ? formatTimestamp(user.otpExpires) : '-'}</div>
+                  </div>
+
+                  <div className="row col-12 viewDataCss">
+                    <div className="col-md-4"><strong>Created Date:</strong> {user.createddate || user.createdDate ? formatTimestamp(user.createddate || user.createdDate) : '-'}</div>
+                    <div className="col-md-4"><strong>Modified Date:</strong> {user.modifieddate || user.modifiedDate ? formatTimestamp(user.modifieddate || user.modifiedDate) : '-'}</div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+        <Footer />
+      </div>
+    </div>
+  </div>
+);
+
+
 };
 
 export default ViewManageUser;
