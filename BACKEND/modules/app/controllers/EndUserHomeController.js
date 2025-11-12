71d96afce496294aa0b8ff5c74be85269152070b
sujatavilaskagle <sujata@outdidtech.com>
2025-11-12 16:31:26 +0530

    updated

--- BACKEND/modules/app/controllers/EndUserHomeController.js
+++ BACKEND/modules/app/controllers/EndUserHomeController.js
@@ -1,6 +1,8 @@
 const { connectToDatabase } = require('../../../config/db');
 const { ObjectId } = require('mongodb');
 
+
+
 exports.getActiveSubscriptionDetails = async (req, res) => {
   const { user_id, email, role_id } = req.body;
 
