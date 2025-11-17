c1aa8419e11d38ebf1e237bc4faae4a89e1f8e2f
sujatavilaskagle <sujata@outdidtech.com>
2025-11-17 11:08:30 +0530

    updated to new branch

--- BACKEND/modules/app/controllers/TechHomecontroller.js
+++ BACKEND/modules/app/controllers/TechHomecontroller.js
@@ -8,6 +8,7 @@ const Razorpay = require('razorpay');
 const crypto = require('crypto');
 const { sendEmail } = require('../../website/controllers/Email');
 
+
 const razorpayInstance = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
